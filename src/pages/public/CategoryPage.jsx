be3b1072c766efae9f0334ecc004297409a71c16
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-09-28 14:20:46 +0500

    update CategoryPage.jsx

--- src/pages/public/CategoryPage.jsx
+++ src/pages/public/CategoryPage.jsx
@@ -24,7 +24,7 @@ const CategoryPage = () => {
     try {
       const response = await request.get(`/post?search=${search}&category=${categoryId}&page=${currentPage}&limit=${pageTotal}`);
       setData(response.data.data);
-      setItemsPerPage(data.pagination.total)
+      setItemsPerPage(response.data.pagination.total)
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -36,15 +36,19 @@ const CategoryPage = () => {
     fetchData(search);
   }, [search]);
 
+  const totalPages = Math.ceil(itemsPerPage / pageTotal);
+
+  const total = data.filter((total) => {
+    console.log(total);
+    const fullName = `${total.title} ${total.description}`;
+    return fullName.toLowerCase().includes(search.toLowerCase());
+  })
+
+  const categoryName = data.length > 0 ? data[0].category.name : '';
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
-
-  const totalPages = Math.ceil(itemsPerPage / pageTotal);
-
-
-
-  const categoryName = data.length > 0 ? data[0].category.name : '';
 
   return (
     <section>
@@ -52,13 +56,18 @@ const CategoryPage = () => {
       <div className="container">
         <input className="search__input" onChange={handleInput} type="text" placeholder="Searching ..." />
       </div>
-      {data.length === 0 ? (
-        <h2 style={{ textAlign: 'center' }}>Not Found Card</h2>
-      ) : (
-        data.map((el, index) => <Card key={index} data={el} />)
+      {search === '' ? (
+        data.length === 0 ? (
+          <h2 style={{ textAlign: 'center' }}>Not Found Card</h2>
+        ) : (
+          data.map((el, index) => <Card key={index} data={el} />)
+        )) : total.length === 0 ? (
+          <h2 style={{ textAlign: 'center' }}>Not Found Card</h2>
+        ) : (
+        total.map((el, index) => <Card key={index} data={el} />)
       )}
       {
-        pageTotal > itemsPerPage ? null : (
+        totalPages > 1 && (
           <div className="category__pagination">
             <button
               className="category__pagination-button"
